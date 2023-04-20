e7103c8536be0c7e375591d1c1984abffb34a4b5
Douglas Bett <bettdalpha@gmail.com>
2023-04-20 11:29:32 +0300

    Add crud examples

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,7 +1,8 @@
 import 'package:dotenv/dotenv.dart';
+import 'package:server/database/models.dart';
 import 'package:stormberry/stormberry.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   final dotEnv = DotEnv(includePlatformEnvironment: true)..load(['.env']);
 
   final database = Database(
@@ -23,4 +24,31 @@ void main(List<String> arguments) {
       () => throw Exception('POSTGRES_DB not defined'),
     ),
   );
+  // Insert
+  await database.users.insertOne(
+    UserInsertRequest(
+      id: 'id',
+      username: 'user1',
+      createdAt: DateTime.now(),
+      passwordHash: 'passwordHash',
+    ),
+  );
+  // query
+  final gotUser = await database.users.queryUser('id');
+  if (gotUser != null) {
+    // user found
+    print(gotUser.id);
+    print(gotUser.username);
+    print(gotUser.createdAt);
+    print(gotUser.phone);
+  }
+
+  /// Update one
+  await database.users.updateOne(
+    UserUpdateRequest(id: 'id', phone: '+254345678'),
+  );
+  final updatedUser = await database.users.queryUser('id');
+
+// delete
+  await database.users.deleteOne('id');
 }
