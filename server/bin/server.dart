c4cca3bdee54115164efe17839856a0e797d8c58
Douglas Bett <bettdalpha@gmail.com>
2023-04-20 11:53:23 +0300

    Add user service

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,54 +1,45 @@
+import 'dart:io';
+
 import 'package:dotenv/dotenv.dart';
+import 'package:grpc/grpc.dart';
 import 'package:server/database/models.dart';
+import 'package:server/interceptors/auth_interceptor.dart';
+import 'package:server/services/user_service.dart';
+import 'package:server/utils/dotenv.dart';
 import 'package:stormberry/stormberry.dart';
 
 void main(List<String> arguments) async {
-  final dotEnv = DotEnv(includePlatformEnvironment: true)..load(['.env']);
-
   final database = Database(
     database: dotEnv.getOrElse(
       'POSTGRES_DB',
       () => throw Exception('POSTGRES_DB not defined'),
     ),
     host: dotEnv.getOrElse(
-      'POSTGRES_DB',
-      () => throw Exception('POSTGRES_DB not defined'),
+      'POSTGRES_HOST',
+      () => throw Exception('POSTGRES_HOST not defined'),
     ),
     port: int.parse(dotEnv.getOrElse(
-      'POSTGRES_DB',
-      () => throw Exception('POSTGRES_DB not defined'),
+      'POSTGRES_PORT',
+      () => throw Exception('POSTGRES_PORT not defined'),
     )),
     useSSL: false,
+    user: dotEnv.getOrElse(
+      'POSTGRES_USER',
+      () => throw Exception('POSTGRES_USER not defined'),
+    ),
     password: dotEnv.getOrElse(
-      'POSTGRES_DB',
-      () => throw Exception('POSTGRES_DB not defined'),
+      'POSTGRES_PASSWORD',
+      () => throw Exception('POSTGRES_PASSWORD not defined'),
     ),
   );
-  // Insert
-  await database.users.insertOne(
-    UserInsertRequest(
-      id: 'id',
-      username: 'user1',
-      createdAt: DateTime.now(),
-      passwordHash: 'passwordHash',
-    ),
-  );
-  // query
-  final gotUser = await database.users.queryUser('id');
-  if (gotUser != null) {
-    // user found
-    print(gotUser.id);
-    print(gotUser.username);
-    print(gotUser.createdAt);
-    print(gotUser.phone);
-  }
 
-  /// Update one
-  await database.users.updateOne(
-    UserUpdateRequest(id: 'id', phone: '+254345678'),
+  final server = Server(
+    [UserService(userRepository: database.users)],
   );
-  final updatedUser = await database.users.queryUser('id');
+  final ip = InternetAddress.anyIPv4;
+  final port = int.parse(dotEnv.getOrElse('PORT', () => '8087'));
 
-// delete
-  await database.users.deleteOne('id');
+  await server.serve(address: ip, port: port);
+
+  print('Server running at $ip on $port');
 }
